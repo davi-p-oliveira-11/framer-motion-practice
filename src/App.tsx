73f62bb631e95482d9f2d299c1820a88c455ac32
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-09-03 14:50:40 -0300

    (feat) added loader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Loader from "./components/Loader"
 import ScrollIndicator from "./components/ScrollIndicator"
 
 const App = () => {
   return (
     <div>
-      <ScrollIndicator />
+      {/* <ScrollIndicator /> */}
+      <Loader />
     </div>
   )
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion";
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
+        className="relative w-16 h-16 border-4 border-t-4 border-blue-500 border-solid rounded-full"
+      >
+        <motion.div
+          animate={{ opacity: [1, 0.5, 1] }}
+          transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
+          style={{ borderTopColor: "transparent" }}
+          className="absolute inset-0 border-4 border-blue-300 border-solid rounded-full"
+        ></motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Loader;
