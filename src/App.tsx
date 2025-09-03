0eb08e4f7186468f8d6042303e013d3715fae94b
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-09-03 17:04:45 -0300

    added DraggableCard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import Loader from "./components/Loader"
-import ScrollIndicator from "./components/ScrollIndicator"
+import DraggableCard from "./components/DraggableCard";
+import Loader from "./components/Loader";
+import ScrollIndicator from "./components/ScrollIndicator";
 
 const App = () => {
   return (
     <div>
       {/* <ScrollIndicator /> */}
-      <Loader />
+      {/* <Loader /> */}
+      <div className="flex justify-center items-center h-[100vh] bg-[#f3f3f3]">
+        <DraggableCard
+          style={{
+            background: "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)",
+          }}
+        >
+          Card 1
+        </DraggableCard>
+        <DraggableCard
+          style={{
+            background: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)",
+          }}
+        >
+          Card 2
+        </DraggableCard>
+        <DraggableCard
+          style={{
+            background: "linear-gradient(135deg, #ff9a9e 0%, #fecfef 100%)",
+          }}
+        >
+          Card 3
+        </DraggableCard>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
