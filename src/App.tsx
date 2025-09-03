8fe427497b9354410002a42ac42df1e3f106e12a
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-09-03 14:27:48 -0300

    (feat) added scroll indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import ScrollIndicator from "./components/ScrollIndicator"
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">App</h1>
-    </>
+    <div>
+      <ScrollIndicator />
+    </div>
   )
 }
 
--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const ScrollIndicator = () => {
-  return (
-    <div>ScrollIndicator</div>
-  )
-}
+  const { scrollYProgress } = useScroll();
+  const lineWidth = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
-export default ScrollIndicator
+  const text: string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            eligendi harum excepturi, nam saepe consectetur rem, fugit dolore
+            adipisci nisi omnis nemo ipsum rerum voluptatem in. Magni cumque
+            ipsa est fuga rem perspiciatis esse vel voluptates voluptatibus nemo
+            nostrum cupiditate modi corporis error neque, molestiae omnis,
+            consequatur itaque dolorum nesciunt, voluptas sint saepe!
+            Voluptates, natus dignissimos? Laborum molestias quis modi sed
+            dignissimos quas assumenda quo explicabo ad, itaque magni quos
+            repellat molestiae esse doloremque odit dolores possimus id nesciunt
+            voluptates nam libero exercitationem natus ipsa? In ipsum aliquam et
+            ducimus consequatur libero consectetur sunt ex, a aperiam, optio
+            vero maiores?`;
+
+  return (
+    <div className="h-[200vh] p=[20px]">
+      <motion.div
+        className="fixed top-0 left-0 h-[5px] bg-red-500"
+        style={{ width: lineWidth, transition: "width 0.1s ease" }}
+      />
+
+      <div className="mt-[50px]">
+        {[...Array(100)].map((_, i) => (
+          <p key={i} className="mt-[20px]">
+            {text}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ScrollIndicator;
